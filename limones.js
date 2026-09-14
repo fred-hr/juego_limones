7a8f78977dd93821f5d78996552aac8dc47d71b4
Freddy Campuzano <frcampuzanoca@uide.edu.ec>
2026-09-14 11:10:03 -0500

    Agregar GAME OVER al quedarse sin vidas

--- limones.js
+++ limones.js
@@ -12,11 +12,12 @@ let personajeY=canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE);
 let limonX=canvas.width/2;
 let limonY=0;
 let puntaje=0;
-let vidas=3;
+let vidas=6;
 let velocidadCaida=200;
 
 function iniciar(){
     setInterval(bajarLimon,velocidadCaida)//1er parametro recibe una funcion, 2do parametro tiempo en milisegundos
+    mostrarSpan("txtVidas",vidas);
     dibujarSuelo();
     dibujarPersonaje();
     aparecerLimon();
@@ -71,6 +72,9 @@ function detectarPiso(){
         aparecerLimon();
         vidas=vidas-1;
         mostrarSpan("txtVidas",vidas);
+        if(vidas==0){
+            alert("GAME OVER")
+        }
     }
 }
 function aparecerLimon(){
