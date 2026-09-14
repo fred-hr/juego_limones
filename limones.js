e8d2123c163a458f673e4f9c68d0093e28cf997c
Freddy Campuzano <frcampuzanoca@uide.edu.ec>
2026-09-14 10:07:37 -0500

    agregar colisiones

--- limones.js
+++ limones.js
@@ -1,13 +1,16 @@
 let canvas=document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
-let personajeX=canvas.width/2;
-let limonX=canvas.width/2;
-let limonY=5;
-const ALTURA_SUELO=50;
+
+const ALTURA_SUELO=20;
 const ALTURA_PERSONAJE=60;
 const ANCHO_PERSONAJE=40;
 const ANCHO_LIMON=20;
-const ALTO_LIMON=20;
+const ALTURA_LIMON=20;
+
+let personajeX=canvas.width/2;
+let personajeY=canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE);
+let limonX=canvas.width/2;
+let limonY=0;
 
 function iniciar(){
     dibujarSuelo();
@@ -20,16 +23,18 @@ function dibujarSuelo(){
 }
 function dibujarPersonaje(){
     ctx.fillStyle="yellow";
-    ctx.fillRect(personajeX,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+    ctx.fillRect(personajeX,personajeY,ANCHO_PERSONAJE,ALTURA_PERSONAJE);
 }
 
 function moverIzquierda(){
     personajeX=personajeX-10;
     actualizarPantalla();
+    detectarColision();
 }
 function moverDerecha(){
     personajeX=personajeX+10;
     actualizarPantalla();
+    detectarColision();
 }
 function actualizarPantalla(){
     limpiarCanvas();
@@ -43,9 +48,15 @@ function limpiarCanvas(){
 }
 function dibujarLimon(){
     ctx.fillStyle="green";
-    ctx.fillRect(limonX,limonY,ANCHO_LIMON,ALTO_LIMON);
+    ctx.fillRect(limonX,limonY,ANCHO_LIMON,ALTURA_LIMON);
 }
 function bajarLimon(){
     limonY=limonY+10;
     actualizarPantalla();
+    detectarColision();
+}
+function detectarColision(){
+    if(limonX+ANCHO_LIMON>personajeX && limonX<personajeX+ANCHO_PERSONAJE && limonY+ALTURA_LIMON>personajeY && limonY<personajeY+ALTURA_PERSONAJE){
+        alert("ATRAPADO!!");
+    }
 }
